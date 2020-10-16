86d284dce8001d7f70e3a6cd5121c3d06671e55c
masonsteeger94 <masonsteeger@gmail.com>
2020-10-15 22:52:24 -0500

    backend CRUD working in postman

--- controllers/jokes_controller.php
+++ controllers/jokes_controller.php
@@ -3,21 +3,21 @@ header('Content-Type: application/json');
 include_once __DIR__ . '/../models/jokes_model.php';
 
 if($_REQUEST['action'] === 'index'){
-    echo json_encode(Joke::all());
+    echo json_encode(Jokes::read());
 }else if ($_REQUEST['action'] === 'create'){
     $request_body = file_get_contents('php://input');
     $body_object = json_decode($request_body);
-    $new_joke = new Joke (null, $body_object->name, $body_object->age);
-    $all_jokes = Joke::create($new_joke);
+    $new_joke = new Joke (null, $body_object->type, $body_object->setup, $body_object->punchline, $body_object->vote);
+    $all_jokes = Jokes::create($new_joke);
     echo json_encode($all_jokes);
 }else if ($_REQUEST['action'] === 'update'){
   $request_body = file_get_contents('php://input');
   $body_object = json_decode($request_body);
-  $updated_joke = new Joke($_REQUEST['id'], $body_object->name, $body_object->age);
-  $all_jokes = Joke::update($updated_joke);
+  $updated_joke = new Joke($_REQUEST['id'], $body_object->type, $body_object->setup, $body_object->punchline, $body_object->vote);
+  $all_jokes = Jokes::update($updated_joke);
   echo json_encode($all_jokes);
 }else if ($_REQUEST['action'] === 'delete'){
-  $all_jokes = Joke::delete($_REQUEST['id']);
+  $all_jokes = Jokes::delete($_REQUEST['id']);
   echo json_encode($all_jokes);
 }
 
---
+++ models/jokes_model.php
@@ -0,0 +1,75 @@
+<?php
+  	$dbconn = pg_connect("host=localhost dbname=jokez");
+
+
+
+
+  class Joke{
+    public $id;
+    public $type;
+    public $setup;
+    public $punchline;
+    public $vote;
+
+
+    public function __construct($id, $type, $setup, $punchline, $vote) {
+      $this->id = $id;
+      $this->type = $type;
+      $this->setup = $setup;
+      $this->punchline = $punchline;
+      $this->vote = $vote;
+    }
+  }
+
+
+
+  class Jokes{
+  static function create($joke){
+    $query = "INSERT INTO jokes (type, setup, punchline, vote) VALUES ($1, $2, $3, $4)";
+    $query_params = array($joke->type, $joke->setup, $joke->punchline, $joke->vote);
+    pg_query_params($query, $query_params);
+    return self::read();
+    }
+
+    static function read(){
+      $jokes = array();
+      $results = pg_query("SELECT * FROM jokes");
+      $row_object = pg_fetch_object($results);
+
+      while($row_object){
+        $new_joke = new Joke(
+          intval($row_object->id),
+          $row_object->type,
+          $row_object->setup,
+          $row_object->punchline,
+          $row_object->vote
+        );
+        $jokes[]= $new_joke;
+
+        $row_object = pg_fetch_object($results);
+      }
+      return $jokes;
+    }
+
+    static function update($updated_joke){
+      $query = "UPDATE jokes SET type = $1, setup=$2, punchline = $3, vote = $4 WHERE id = $5";
+      $query_params = array($updated_joke->type, $updated_joke->setup, $updated_joke->punchline, $updated_joke->vote, $updated_joke->id);
+      pg_query_params($query, $query_params);
+
+      return self::read();
+    }
+
+
+    static function delete($id){
+      $query = "DELETE FROM jokes WHERE id = $1";
+      $query_params = array($id);
+      pg_query_params($query, $query_params);
+
+      return self::read();
+    }
+
+  }
+
+
+
+ ?>
