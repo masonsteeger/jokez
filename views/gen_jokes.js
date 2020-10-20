24bc67e6619a449a65ded94a00d0241072cfdbc3
Jc7 <jhuinac7@gmail.com>
2020-10-19 22:22:04 -0700

    more styling for the library

--- views/gen_jokes.js
+++ views/gen_jokes.js
@@ -1,25 +1,25 @@
-class GenJokes extends React.Component{
+class GenJokes extends React.Component {
 
 
   render = () => {
-    let genJokes = this.props.state.jokes.filter(function (joke){
-    return joke.type === 'general'
-  })
-  return(
-    <div className="gen-container">
-      {genJokes.map(
-        (joke) => {
-          return (
-            <div className="joke-div" key={joke.id}>
-              <h2>{joke.type}</h2>
-              <h4>{joke.setup}</h4><br/>
-              <h4>{joke.punchline}</h4><br/>
-              <p><div className='vote-up' id={joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteUp}></div> {joke.vote} <div className='vote-down' id = {joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteDown}></div></p>
-            </div>
-          )
-        }
-      )}
-    </div>
-  )
+    let genJokes = this.props.state.jokes.filter(function (joke) {
+      return joke.type === 'general'
+    })
+    return (
+      <div className="gen-container">
+        {genJokes.map(
+          (joke) => {
+            return (
+              <div className="joke-div" key={joke.id}>
+                <h2>{joke.type}</h2>
+                <h4>{joke.setup}</h4><br />
+                <h5>{joke.punchline}</h5><br />
+                <div><div className='vote-up' id={joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteUp}></div>  <div className='vote-down' id={joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteDown}></div>{(joke.vote >= 0) ? <p className="green">{joke.vote}</p> : <p className="red">{joke.vote}</p>}</div>
+              </div>
+            )
+          }
+        )}
+      </div>
+    )
   }
 }
--- views/knock.js
+++ views/knock.js
@@ -1,23 +1,23 @@
-class KnockJokes extends React.Component{
+class KnockJokes extends React.Component {
   render = () => {
-    let knockJokes = this.props.state.jokes.filter(function (joke){
-    return joke.type === "knock-knock"
-  })
-  return(
-    <div className="knock-container">
-      {knockJokes.map(
-        (joke) => {
-          return (
-            <div className="joke-div" key={joke.id}>
-              <h2>{joke.type}</h2>
-              <h4>{joke.setup}</h4><br/>
-              <h4>{joke.punchline}</h4><br/>
-              <p><div className='vote-up' id={joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteUp}></div> {joke.vote} <div className='vote-down' id = {joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteDown}></div></p>
-            </div>
-          )
-        }
-      )}
-    </div>
-  )
+    let knockJokes = this.props.state.jokes.filter(function (joke) {
+      return joke.type === "knock-knock"
+    })
+    return (
+      <div className="knock-container">
+        {knockJokes.map(
+          (joke) => {
+            return (
+              <div className="joke-div" key={joke.id}>
+                <h2>{joke.type}</h2>
+                <h4>{joke.setup}</h4><br />
+                <h5>{joke.punchline}</h5><br />
+                <div><div className='vote-up' id={joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteUp}></div> <div className='vote-down' id={joke.id} joketype={joke.type} jokesetup={joke.setup} jokepunchline={joke.punchline} value={joke.vote} onClick={this.props.voteDown}></div>{(joke.vote >= 0) ? <p className="green">{joke.vote}</p> : <p className="red">{joke.vote}</p>}</div>
+              </div>
+            )
+          }
+        )}
+      </div>
+    )
   }
 }
